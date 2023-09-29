1478f901fccac236ebee19d4ac0112c7130fb7a2
Jaimilit <>
2023-09-29 14:40:51 +0000

    retry

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "template_jaimicv", {
+    emailjs.send("gmail", "rosie", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
@@ -14,4 +14,3 @@ function sendMail(contactForm) {
         );
     return false;  // To block from loading a new page
 }
-
